d0698012c39c50e23b59a33048bb134f09e30da4
Ravi Nath <majindm24@gmail.com>
2020-01-28 20:55:36 -0500

    initial commit

---
+++ pokewtt/src/app/pokelist/pokelist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokelist',
+  templateUrl: './pokelist.component.html',
+  styleUrls: ['./pokelist.component.sass']
+})
+export class PokelistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
